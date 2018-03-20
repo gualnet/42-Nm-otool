aa214365ab4e44c9afbfc5ae03e68e61d21a5be9
galnet <galy@student.42.fr>
2018-03-20 20:42:22 +0100

    XXXX diff in the value printed

--- Nm/src/display.c
+++ Nm/src/display.c
@@ -6,7 +6,7 @@
 /*   By: galy <galy@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/14 15:16:41 by galy              #+#    #+#             */
-/*   Updated: 2018/03/20 19:18:37 by galy             ###   ########.fr       */
+/*   Updated: 2018/03/20 20:35:35 by galy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,9 @@ void	lssi_2(t_lc_lnk *lc_lnk, struct load_command *lc)
 	segcmd = (void*)lc;
 	seccmd = (void*)lc + sizeof(struct segment_command_64);
 	i = 0;
-	
 	// ft_printf("segcmd->nsects [%d]\n",segcmd->nsects);
 	while (i < segcmd->nsects)
 	{
-
 		// ft_printf("\nseccmd->segname [%s]\n",seccmd->segname);
 		// ft_printf("seccmd->sectname [%s]\n",seccmd->sectname);
 		if ((add_new_sectlnk(lc_lnk, seccmd)) == NULL)
@@ -41,7 +39,6 @@ void	lssi_2(t_lc_lnk *lc_lnk, struct load_command *lc)
 void	load_seg_sect_inlist(t_vault *vault)
 {
 	t_lc_lnk	*tmp;
-	// struct load_command	*lc;	
 
 	tmp = vault->lc_lst;
 	// ft_printf("vault->lc_lst [%p][%p][%p]\n",&vault->lc_lst,vault->lc_lst,*vault->lc_lst);
@@ -59,41 +56,26 @@ void	load_seg_sect_inlist(t_vault *vault)
 void	display_list(t_vault *vault)
 {
 	ft_printf("\nCALL DISPLAY_LIST\n");
-	
 	unsigned int	i;
 	int j;
 	char letter;
 	
 	i = 0;
 	j = 0;
-	// ft_printf("vault->nsyms %d\n", vault->nsyms);
 	load_seg_sect_inlist(vault);
 	while (i < vault->nsyms)
-	{
-		
+	{		
 		letter = '?';
-		// ft_printf("001\n");
-		// ft_printf("vault->tab_sym_meta[%d]	->name[%s] \n",i, vault->tab_sym_meta[i]->name);
-		// ft_printf("vault->tab_sym_meta[%d]	->n_value[%s] \n",i, vault->tab_sym_meta[i]->n_value);
-		// ft_printf("vault->tab_sym_meta[%d]	->n_sect[0x0%x][%b] \n",i, vault->tab_sym_meta[i]->n_sect ,vault->tab_sym_meta[i]->n_sect);
-		// ft_printf("vault->tab_sym_meta[%d]	->n_type[0x0%x][%b] \n",i, vault->tab_sym_meta[i]->n_type ,vault->tab_sym_meta[i]->n_type);
 		letter = print_sym_sect(vault, i);
-		// ft_printf("002\n");
+		
 		if (letter != 'U')
-			ft_printf("%x ",vault->tab_sym_meta[i]->n_value);
+			ft_printf("%016llx ",vault->tab_sym_meta[i]->n_value);
 		else
 			ft_printf("%-17s", "");
-		// ft_printf("003\n");
 
 		ft_printf("%c ", letter);
-
 		ft_printf("%s\n", vault->tab_sym_meta[i]->name);
-		// ft_printf("004\n");
-		// ft_printf("vault->tab_sym_meta[%d]	->n_sect[0x0%x][%b] \n",vault->tab_sym_meta[i]->n_sect ,vault->tab_sym_meta[i]->n_sect);
-		// ft_printf("vault->tab_sym_meta[%d]	->n_type[0x0%x][%b] \n",vault->tab_sym_meta[i]->n_type ,vault->tab_sym_meta[i]->n_type);
-		
 		i++;
-		// ft_printf("\n");
 	}
 	ft_printf("\nEND DISPLAY_LIST\n");
 }
--- Nm/src/iter_cmds.c
+++ Nm/src/iter_cmds.c
@@ -6,7 +6,7 @@
 /*   By: galy <galy@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/23 21:36:21 by galy              #+#    #+#             */
-/*   Updated: 2018/03/20 14:25:09 by galy             ###   ########.fr       */
+/*   Updated: 2018/03/20 20:35:30 by galy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,10 +54,12 @@ void	symtab_loop(t_vault *vault, struct symtab_command *symtab_cmd, void *strtab
 			vault->tab_sym_meta[j]->n_sect = nlist[i].n_sect;
 			vault->tab_sym_meta[j]->n_type = nlist[i].n_type;
 			vault->tab_sym_meta[j]->n_value = nlist[i].n_value;
+			ft_printf("%016x %s\n", vault->tab_sym_meta[j]->n_value, vault->tab_sym_meta[j]->name);
 			j++;
 		}
 		i++;
 	}
+	// exit(0);
 }
 
 void	handle_symtab(t_vault *vault, struct load_command *lc)
